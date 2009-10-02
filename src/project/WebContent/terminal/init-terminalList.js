583a003618a7a0f6060dfd1ea25b1aa8323252e5
lujuping <lujuping@43b2a52a-4c18-11de-8d14-a1ddb34ee23c>
2009-10-02 15:37:05 +0000

    for terminal module

---
+++ src/project/WebContent/terminal/init-terminalList.js
@@ -0,0 +1,134 @@
+var ds;// 数据源
+var grid;// 数据显示表格
+var searchForm;// 查询表单
+var limit = 5;// 每页显示的记录数
+var ptb;// 分页控件
+
+// 页面加载后执行的代码
+Ext.onReady(function() {
+	Ext.BLANK_IMAGE_URL="resources/images/default/s.gif";
+	initData();
+});
+
+// 初始化数据
+function initData() {
+	ds = new Ext.data.Store({
+		proxy : new Ext.data.HttpProxy({
+			url : 'terminal_search.do'
+		}),
+		reader : new Ext.data.JsonReader({
+			root : 'root'
+		}, [{
+			name : 'terminalId'
+		}, {
+			name : 'terminalName'
+		}, {
+			name : 'terminalNumber'
+		}, {
+			name : 'terminalProtocol'
+		}, {
+			name : 'maxBandwidth'
+		}])
+	});
+	ds.load();
+	initGrid();
+}
+// 初始化显示表格
+function initGrid() {
+	Ext.QuickTips.init();
+	var sm = new Ext.grid.CheckboxSelectionModel();
+
+	var cm = new Ext.grid.ColumnModel([sm, {
+		dataIndex : 'terminalId',
+		hidden : true
+	},{
+		header : "终端名称",
+		width: Ext.get("searchArea").getWidth() * 0.25,
+		sortable : true,
+		dataIndex : 'terminalName',
+		renderer : function(value, p , record){
+			return String.format('<a href="terminal_detail.do?terminalId={0}" target="_blank">{1}</a>',record.data.terminalId,value);
+		}
+	}, {
+		header : "呼叫号码",
+		width: Ext.get("searchArea").getWidth() * 0.25,
+		sortable : true,
+		dataIndex : 'terminalNumber'
+	}, {
+		header : "终端类型",
+		width: Ext.get("searchArea").getWidth() * 0.25,
+		sortable : false,
+		dataIndex : 'terminalProtocol'
+	}, {
+		header : "带宽",
+		width: Ext.get("searchArea").getWidth() * 0.25,
+		sortable : true,
+		dataIndex : 'maxBandwidth'
+	}]);
+	// 工具栏对象
+	ptb = new Ext.PagingToolbar({
+		plugins : new Ext.ux.Andrie.pPageSize(),
+		pageSize : limit,
+		store : ds,
+		displayInfo : true,
+		displayMsg : '显示第 {0} 条到 {1} 条记录，一共 {2} 条',
+		emptyMsg : "对不起，查询记录为空!",
+		doLoad : loadStore
+		,items:['-', { 
+					pressed: true, 
+					enableToggle:true, 
+					text:'重置列表宽度 ', 
+					tooltip : '重置列表宽度',
+					iconCls : 'resetWidth16',
+					onClick : function() {
+						window.location.reload(); 
+					} 
+				}] 
+
+	});
+	// 表格对象
+	grid = new Ext.grid.GridPanel({
+		id : 'button-grid',
+		border : false,
+		buttonAlign : 'right',
+		ds : ds,
+		cm : cm,
+		sm : sm,
+		stripeRows : true,
+		forceFit:true,
+		loadMask : true,
+		tbar : ['<b>&nbsp;&nbsp;&nbsp;&nbsp;<font color=#990000>终端列表</font></b>','->',{
+			id : 'btpdate',
+			text : '更新',
+			pressed : true,
+			tooltip : '更新终端列表',
+			iconCls : 'add16',
+			onClick : function() {
+				location.href = "terminal_update.do";
+			}
+		}],
+		bbar : ptb,
+		width: Ext.get("searchArea").getWidth()*0.99,
+		height:Ext.get("searchArea").getHeight()*0.99,
+		autoHeight : true,
+		renderTo : 'searchArea'
+	});
+}
+
+function query() {
+	loadStore(0);
+}
+function loadStore(start){
+	ds.load({
+		params : {
+			start : start,
+			limit : ptb.getPageSize(),
+			'totalProperty' : ds.getTotalCount(),
+			'terminalName' : Ext.get("terminalName").dom.value,
+			'terminalNumber' : Ext.get("terminalNumber").dom.value
+		}
+	});
+}
+function reset() {
+	Ext.get("form1").reset();
+}
---
+++ src/project/WebContent/terminal/list.jsp
@@ -0,0 +1,54 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>终端列表</title>
+<link rel="stylesheet" type="text/css" href="<%=request.getContextPath() %>/resources/css/ext-all.css"/>
+<link rel="stylesheet" type="text/css" href="<%=request.getContextPath() %>/resources/css/content.css"/>
+<link rel="stylesheet" type="text/css" href="<%=request.getContextPath() %>/resources/css/edgrid.css"/>
+<script type="text/javascript" src="<%=request.getContextPath() %>/resources/js/ext-base.js"></script>
+<script type="text/javascript" src="<%=request.getContextPath() %>/resources/js/ext-all.js"></script>
+<script type="text/javascript" src="<%=request.getContextPath() %>/resources/js/ext-lang-zh_CN.js"></script>
+<script type="text/javascript" src="<%=request.getContextPath() %>/resources/js/pager.js"></script>
+<script type="text/javascript" src="<%=request.getContextPath() %>/dwr/engine.js"></script>
+<script type="text/javascript" src="<%=request.getContextPath() %>/dwr/util.js"></script>
+<script type="text/javascript" src="<%=request.getContextPath() %>/dwr/interface/terminalService.js"></script>
+<script type="text/javascript" src="<%=request.getContextPath() %>/service/init-terminalList.js" defer="defer"></script>
+<style type="text/css">
+.STYLE3 {
+	font-size: 12px
+}
+
+#button-grid .x-panel-body {
+	border: 1px solid #99bbe8;
+	border-top: 0 none;
+}
+</style>
+</head>
+<body>
+<div class="wrap">
+	<h1>当前位置：基本信息&nbsp;&gt;&nbsp;<span class="position_current"> 终端列表</span></h1>
+		<div id="searchArea" class="search">
+			<form name="form1" id="form1" method="post" action="<%=request.getContextPath()%>/terminal_search.do">
+				<table border="1" width="800" class="query">
+					<tr>
+						<th>终端名称：</th>
+						<td>
+							<input name="terminalName" type="text" id="terminalName" class="put200" maxlength="15"/>
+						</td>
+						<th>终端号：</th>
+						<td>
+							<input name="terminalNumber" type="text" id="terminalNumber" class="put200" maxlength="15"/>
+						</td>
+					</tr>
+				</table>
+				<div align="center" class="query_btn">
+					<td colspan="2">
+						<input type="button" value="查询" id="btnQuery"	class="butt_bg1" onMouseOver="this.className='butt_bg1_over'" onMouseOut="this.className='butt_bg1'" onClick="query()" /> &nbsp;&nbsp;
+						<input type="button" value="重置" id="btnReset"	class="butt_bg1" onMouseOver="this.className='butt_bg1_over'" onMouseOut="this.className='butt_bg1'" onClick="reset()" /></td>
+				</div>
+			</form>
+		</div><!--end of searchArea-->
+	</div><!--end of wrap-->
+</body>
+</html>
