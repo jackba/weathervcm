a686b8de970aead7e8b67612dcade9f5b4a310a0
lujuping <lujuping@43b2a52a-4c18-11de-8d14-a1ddb34ee23c>
2009-10-02 15:36:13 +0000

    for terminal module

---
+++ trunk/src/project/src/com/cma/intervideo/dao/AbstractTerminalDao.java
@@ -0,0 +1,44 @@
+package com.cma.intervideo.dao;
+
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.cma.intervideo.dao.util.AbstractDAO;
+import com.cma.intervideo.pojo.Terminal;
+import com.cma.intervideo.util.PageHolder;
+import com.cma.intervideo.util.ParamVo;
+
+public class AbstractTerminalDao extends AbstractDAO<Terminal, String>
+		implements ITerminalDao {
+	private final static Log logger = LogFactory
+			.getLog(AbstractTerminalDao.class);
+
+	public List<Terminal> findTerminals(List<ParamVo> params, PageHolder ph) {
+		String hql = "from terminal t order by t.terminalId";
+		if(params!=null){
+			for(int i=0;i<params.size();i++){
+				ParamVo vo = params.get(i);
+				if(vo.getParamName().equals("terminalName")){
+					hql += " and t.terminalName like '%"+vo.getParamValue()+"%'";
+				}
+				if(vo.getParamName().equals("terminalNumber")){
+					hql += " and t.terminalNumber='"+vo.getParamValue()+"'";
+				}
+			}
+		}
+		if(ph!=null && ph.isGetCount()){
+			ph.setResultSize(this.getCount(hql));
+		}
+		List<Terminal> lst = getHibernateTemplate().find(hql);
+		logger.info("AbstractTerminalDao.findTerminals return " + lst.size()
+				+ " Terminal, hql = " + hql);
+		return lst;
+	}
+
+	public Terminal getTerminal(String terminalId) {
+		return (Terminal) getHibernateTemplate().get(
+				Terminal.class, terminalId);
+	}
+}
---
+++ trunk/src/project/src/com/cma/intervideo/service/impl/TerminalServiceImpl.java
@@ -0,0 +1,33 @@
+package com.cma.intervideo.service.impl;
+
+import java.util.List;
+
+import com.cma.intervideo.dao.ITerminalDao;
+import com.cma.intervideo.pojo.Terminal;
+import com.cma.intervideo.service.ITerminalService;
+import com.cma.intervideo.util.PageHolder;
+import com.cma.intervideo.util.ParamVo;
+
+public class TerminalServiceImpl implements ITerminalService{
+	private ITerminalDao terminalDao;
+	public void setTerminalDao(ITerminalDao terminalDao) {
+		this.terminalDao = terminalDao;
+	}
+	public List<Terminal> findTerminals(List<ParamVo> params, PageHolder ph){
+		return terminalDao.findTerminals(params, ph);
+	}
+	public void saveOrUpdate(Terminal terminal){
+		terminalDao.saveOrUpdate(terminal);
+	}
+	public int deleteAllTerminals() {
+		List<Terminal> lst = findTerminals(null, null);
+		int ret = lst == null ? 0 : lst.size();
+		for (int i = 0; lst != null && i < lst.size(); i++) {
+			terminalDao.removeObjectByID(lst.get(i).getTerminalId());
+		}
+		return ret;
+	}
+	public Terminal getTerminal(String terminalId) {
+		return terminalDao.getTerminal(terminalId);
+	}
+}
