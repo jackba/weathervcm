fa430eed0503e82407594a31fcb6b6464693e254
lujuping <lujuping@43b2a52a-4c18-11de-8d14-a1ddb34ee23c>
2009-10-02 15:32:47 +0000

    for terminal module

--- src/project/src/com/cma/intervideo/dao/mysql/BulletinDaoImpl.java
+++ src/project/src/com/cma/intervideo/dao/mysql/BulletinDaoImpl.java
@@ -1,9 +1,6 @@
 package com.cma.intervideo.dao.mysql;
 
-import java.util.List;
-
 import com.cma.intervideo.dao.AbstractBulletinDao;
-import com.cma.intervideo.pojo.BulletinBoard;
 
 public class BulletinDaoImpl extends AbstractBulletinDao{
 	
